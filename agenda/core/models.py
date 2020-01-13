578a48d39523fd3a76255775b2b32c513973a0be
Thiago Ferreira <ferreirathiago@me.com>
2020-01-13 17:27:51 -0300

    Adicionado Editar e Excluir eventos

--- agenda/core/models.py
+++ agenda/core/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Evento(models.Model):
     titulo = models.CharField(max_length=100)
+    local = models.CharField(max_length=100, blank=True, null=True)
     descricao = models.TextField(blank=True, null=True)
     data_evento  = models.DateTimeField(verbose_name='Data do Evento')
     data_criacao = models.DateTimeField(auto_now=True)
@@ -17,3 +18,7 @@ class Evento(models.Model):
 
     def get_data_evento(self):
         return self.data_evento.strftime('%d/%m/%Y às %H:%M')
+
+#Função padrão do datatime-local = strftime('%Y-%m-%dT%H:%M')
+    def get_data_input_evento(self):
+        return self.data_evento.strftime('%Y-%m-%dT%H:%M')
