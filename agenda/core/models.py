4d2b7766b4b49c53ef2d7bd8110fbddc41589d04
Thiago Ferreira <ferreirathiago@me.com>
2020-01-13 11:36:19 -0300

    Commit tela de listagem de eventos

--- agenda/core/models.py
+++ agenda/core/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Evento(models.Model):
+    titulo = models.CharField(max_length=100)
+    descricao = models.TextField(blank=True, null=True)
+    data_evento  = models.DateTimeField(verbose_name='Data do Evento')
+    data_criacao = models.DateTimeField(auto_now=True)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    class Meta:
+        db_table = 'evento' #Obrigando o nome da tabela seja evento
+
+    #Colocar o titulo como nome do Evento
+    def __str__(self):
+        return self.titulo
+
+    def get_data_evento(self):
+        return self.data_evento.strftime('%d/%m/%Y às %H:%M')
